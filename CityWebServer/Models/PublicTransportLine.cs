c27781a95097163536c8cae351eaf42de97adeb8
RenaKunisaki <hyperhacker@gmail.com>
2019-04-08 17:00:34 -0400

    finally send transit line names

--- CityWebServer/Models/PublicTransportLine.cs
+++ CityWebServer/Models/PublicTransportLine.cs
@@ -1,15 +1,19 @@
 using System;
+using System.Collections.Generic;
 
-namespace CityWebServer.Models
-{
-    public class PublicTransportLine
-    {
-        public String Name { get; set; }
-
-        public int VehicleCount { get; set; }
-
-        public int StopCount { get; set; }
-
-        public PopulationGroup[] Passengers { get; set; }
-    }
+namespace CityWebServer.Models {
+	public class PublicTransportLine {
+		public string name;
+		public string type;
+		public int lineNumber;
+		public int vehicleCount;
+		public int stopCount;
+		public bool complete;
+		public bool activeDay, activeNight;
+		public float[] color;
+		public ushort ticketPrice;
+		public float totalLength;
+		public int maintenanceCostPerVehicle;
+		public Dictionary<string, uint> passengers;
+	}
 }
--- CityWebServer/SocketHandlers/TransportHandler.cs
+++ CityWebServer/SocketHandlers/TransportHandler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using CityWebServer.Callbacks;
 using CityWebServer.Extensibility;
 //using CityWebServer.Extensibility.Responses;
 using CityWebServer.Models;
@@ -16,67 +17,126 @@ namespace CityWebServer.SocketHandlers {
 	public class TransportHandler: SocketHandlerBase {
 		public TransportHandler(SocketRequestHandler handler) :
 		base(handler, "Transport") {
+			handler.RegisterMessageHandler("Transport", OnClientMessage);
 		}
 
-		protected void SendAll() {
+		/// <summary>
+		/// Handle "Transport" message from client.
+		/// </summary>
+		/// <param name="_param">Parameter.</param>
+		/// <remarks>Expects a dict with one of the keys:
+		/// <c>get</c>: line ID => get info about specified line
+		/// <c>list</c>: (anything) => get list of valid IDs
+		/// </remarks>
+		public void OnClientMessage(SocketMessageHandlerParam _param) {
+			var param = _param.param as Dictionary<string, object>;
+			var key = param.Keys.First();
+			switch(key) {
+				case null:
+					SendErrorResponse(HttpStatusCode.BadRequest);
+					break;
+				case "get":
+					int? id = param["get"] as int?;
+					if(id == null) {
+						SendErrorResponse("Invalid line ID");
+						return;
+					}
+					SendLine((int)id);
+					break;
+				case "list":
+					SendList();
+					break;
+				default:
+					SendErrorResponse("Transport has no method '{key}'");
+					break;
+			}
+		}
+
+		/// <summary>
+		/// Send a list of valid line IDs.
+		/// </summary>
+		protected void SendList() {
 			var transportManager = Singleton<TransportManager>.instance;
 			if(transportManager == null) {
 				SendErrorResponse(HttpStatusCode.ServiceUnavailable);
 				return;
 			}
-			//LogMessage("got transportManager");
-
 			//somewhere in here we're getting a null reference
 			//if the game hasn't finished loading yet
 
+			List<int> ids = new List<int>();
 			TransportLine[] lines;
-			List<PublicTransportLine> lineModels;
 			try {
 				lines = transportManager.m_lines.m_buffer;
-				lineModels = new List<PublicTransportLine>();
 			}
 			catch(Exception ex) {
 				Log($"Error getting transport lines buffer: {ex}");
 				throw;
 			}
 
-			foreach(var line in lines) {
-				try {
-					if(line.m_flags == TransportLine.Flags.None) { continue; }
-
-					var passengers = line.m_passengers;
-					List<PopulationGroup> passengerGroups = new List<PopulationGroup> {
-						new PopulationGroup("Child", (int) passengers.m_childPassengers.m_finalCount),
-						new PopulationGroup("Teen", (int) passengers.m_teenPassengers.m_finalCount),
-						new PopulationGroup("YoungAdult", (int) passengers.m_youngPassengers.m_finalCount),
-						new PopulationGroup("Adult", (int) passengers.m_adultPassengers.m_finalCount),
-						new PopulationGroup("Senior", (int) passengers.m_seniorPassengers.m_finalCount),
-						new PopulationGroup("Tourist", (int) passengers.m_touristPassengers.m_finalCount),
-						new PopulationGroup("Resident", (int) passengers.m_residentPassengers.m_finalCount),
-						new PopulationGroup("CarOwning", (int) passengers.m_carOwningPassengers.m_finalCount)
-					};
-
-					var stops = line.CountStops(0); // The parameter is never used.
-					var vehicles = line.CountVehicles(0); // The parameter is never used.
-
-					var lineModel = new PublicTransportLine {
-						Name = $"{line.Info.name} {line.m_lineNumber}",
-						StopCount = stops,
-						VehicleCount = vehicles,
-						Passengers = passengerGroups.ToArray(),
-					};
-					lineModels.Add(lineModel);
-				}
-				catch(System.NullReferenceException) {
-					//Game isn't loaded yet.
-					//XXX pinpoint where this error happens.
-					SendErrorResponse(HttpStatusCode.ServiceUnavailable);
-				}
+			for(int i = 0; i < lines.Length; i++) {
+				var line = lines[i];
+				if(line.m_flags == TransportLine.Flags.None) continue;
+				ids.Add(i);
 			}
 
 			//LogMessage("Transport: ordering");
-			lineModels = lineModels.OrderBy(obj => obj.Name).ToList();
-			SendJson(lineModels);
+			//lineModels = lineModels.OrderBy(obj => obj.Name).ToList();
+			SendJson(ids, "TransportIDs");
+		}
+
+		/// <summary>
+		/// Send info about a transport line.
+		/// </summary>
+		/// <param name="id">Line ID.</param>
+		protected void SendLine(int id) {
+			SendJson(GetLine(id));
+		}
+
+		/// <summary>
+		/// Get info about specified line by ID.
+		/// </summary>
+		/// <returns>The line.</returns>
+		/// <param name="id">Line ID.</param>
+		protected CityWebServer.Models.PublicTransportLine GetLine(int id) {
+			var transportManager = Singleton<TransportManager>.instance;
+			var instanceManager = Singleton<InstanceManager>.instance;
+			if(transportManager == null || instanceManager == null) {
+				//SendErrorResponse(HttpStatusCode.ServiceUnavailable);
+				return null;
+			}
+			var line = transportManager.m_lines.m_buffer[id];
+			if(line.m_flags == TransportLine.Flags.None) return null;
+
+			var passengers = line.m_passengers;
+			line.GetActive(out bool day, out bool night);
+			var color = line.GetColor();
+
+			return new PublicTransportLine {
+				//name = line.Info.name,
+				name = transportManager.GetLineName((ushort)id),
+				type = line.Info.name,
+				lineNumber = line.m_lineNumber,
+				vehicleCount = line.CountVehicles(0), // The parameter is never used.
+				stopCount = line.CountStops(0), // The parameter is never used.
+				complete = line.Complete,
+				activeDay = day,
+				activeNight = night,
+				color = new float[] { color.r, color.g, color.b, color.a },
+				ticketPrice = line.m_ticketPrice,
+				totalLength = line.m_totalLength,
+				maintenanceCostPerVehicle = line.Info.m_maintenanceCostPerVehicle,
+				passengers = new Dictionary<string, uint> {
+					{"Child", passengers.m_childPassengers.m_finalCount},
+					{"Teen", passengers.m_teenPassengers.m_finalCount},
+					{"YoungAdult", passengers.m_youngPassengers.m_finalCount},
+					{"Adult", passengers.m_adultPassengers.m_finalCount},
+					{"Senior", passengers.m_seniorPassengers.m_finalCount},
+					{"Tourist", passengers.m_touristPassengers.m_finalCount},
+					{"Resident", passengers.m_residentPassengers.m_finalCount},
+					{"CarOwning", passengers.m_carOwningPassengers.m_finalCount},
+				},
+			};
 		}
 	}
 }
